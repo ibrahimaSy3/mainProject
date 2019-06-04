cba2624dbde1bdaa8644096138235e58ebce778b
LaurentJeanZephirin <jeanzephirin93@gmail.com>
2019-06-04 10:25:56 +0200

    add exercises

---
+++ apiNode/dist/cvPug/functions/src/hostel.js
@@ -0,0 +1,189 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hostels = [
+    {
+        id: 1,
+        name: 'hotel rose',
+        roomNumbers: 10,
+        pool: true,
+        rooms: [
+            {
+                roomName: 'suite de luxe',
+                size: 2,
+                id: 1
+            },
+            {
+                roomName: 'suite nuptiale',
+                size: 2,
+                id: 2
+            },
+            {
+                roomName: 'suite familiale',
+                size: 4,
+                id: 3
+            },
+            {
+                roomName: 'suite budget',
+                size: 2,
+                id: 4
+            },
+            {
+                roomName: 'suite familiale',
+                size: 4,
+                id: 5
+            },
+            {
+                roomName: 'suite budget',
+                size: 3,
+                id: 6
+            },
+            {
+                roomName: 'suite de luxe',
+                size: 2,
+                id: 7
+            },
+            {
+                roomName: 'suite familiale',
+                size: 4,
+                id: 8
+            },
+            {
+                roomName: 'suite de luxe',
+                size: 3,
+                id: 9
+            },
+            {
+                roomName: 'suite présidentielle',
+                size: 5,
+                id: 10
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: 'hotel ocean',
+        roomNumbers: 15,
+        pool: false,
+        rooms: [
+            {
+                roomName: 'suite pacifique',
+                size: 2,
+                id: 1
+            },
+            {
+                roomName: 'suite atlantique',
+                size: 2,
+                id: 2
+            },
+            {
+                roomName: 'suite manche',
+                size: 4,
+                id: 3
+            },
+            {
+                roomName: 'suite mer du nord',
+                size: 2,
+                id: 4
+            },
+            {
+                roomName: 'suite pacifique',
+                size: 4,
+                id: 5
+            },
+            {
+                roomName: 'suite mer du nord',
+                size: 3,
+                id: 6
+            },
+            {
+                roomName: 'suite atlantique',
+                size: 2,
+                id: 7
+            },
+            {
+                roomName: 'suite pacifique',
+                size: 4,
+                id: 8
+            },
+            {
+                roomName: 'suite atlantique',
+                size: 3,
+                id: 9
+            },
+            {
+                roomName: 'suite atlantique',
+                size: 5,
+                id: 10
+            },
+            {
+                roomName: 'suite pacifique',
+                size: 2,
+                id: 11
+            },
+            {
+                roomName: 'suite mer du nord',
+                size: 2,
+                id: 12
+            },
+            {
+                roomName: 'suite manche',
+                size: 4,
+                id: 13
+            },
+            {
+                roomName: 'suite manche',
+                size: 3,
+                id: 14
+            },
+            {
+                roomName: 'suite mer du nord',
+                size: 5,
+                id: 15
+            },
+        ]
+    },
+    {
+        id: 3,
+        name: 'hotel des Pins',
+        roomNumbers: 7,
+        pool: true,
+        rooms: [
+            {
+                roomName: 'suite bordelaise',
+                size: 2,
+                id: 1
+            },
+            {
+                roomName: 'suite marseillaise',
+                size: 2,
+                id: 2
+            },
+            {
+                roomName: 'suite nicoise',
+                size: 4,
+                id: 3
+            },
+            {
+                roomName: 'suite canoise',
+                size: 2,
+                id: 4
+            },
+            {
+                roomName: 'suite hendaiar',
+                size: 4,
+                id: 5
+            },
+            {
+                roomName: 'suite canoise',
+                size: 3,
+                id: 6
+            },
+            {
+                roomName: 'suite nicoise',
+                size: 2,
+                id: 7
+            }
+        ]
+    }
+];
+//# sourceMappingURL=hostel.js.map
